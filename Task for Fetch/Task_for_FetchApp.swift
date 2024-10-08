5c91b1ef7710a1f7a800a1932b0fac052974ef3e
Artashes Ghazaryan <a.ghazaryan@arammeem.com>
2024-10-08 13:32:47 -0500

    Initial Commit

---
+++ Task for Fetch/Task_for_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_for_FetchApp.swift
+//  Task for Fetch
+//
+//  Created by Artashes Ghazaryan on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_for_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
